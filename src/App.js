d6813f2bc57796a699e0219585acfe418d63be13
EKaxada <enocksdha@gmail.com>
2021-02-10 17:01:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = (props) => {
+  const [file, setFile] = useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("myfile", file);
+    const URL = "https://file-server-side.herokuapp.com/upload";
+    axios
+      .post(URL, formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => alert("file successfully uploaded"))
+      .catch((err) => console.error(err));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit}>
+      <h1>upload your file here</h1>
+      <input
+        type="file"
+        name="image"
+        onchange={(e) => setFile(e.target.files)}
+      />
+      <button type="submit">upload</button>
+    </form>
   );
-}
+};
 
 export default App;
